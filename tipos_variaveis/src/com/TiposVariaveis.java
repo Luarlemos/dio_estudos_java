be1c6febc020d0eeda7ffc87a2f168efcb110300
Luarlemos <luarlemos96@hotmail.com>
2023-02-27 18:29:01 -0300

    update projeto tipos_variaveis

--- tipos_variaveis/src/com/TiposVariaveis.java
+++ tipos_variaveis/src/com/TiposVariaveis.java
@@ -5,6 +5,23 @@ public class TiposVariaveis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		
+		// tipos primitivos de variáveis
+		
+		String meuNome = "Luciana Lemos";
+		
+		double salarioMinimo = 2500;
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero = 5;
+		
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		// Constante é escrita em caixa alta
+		final double VALOR_DE_PI = 3.14;
 	}
-
 }
